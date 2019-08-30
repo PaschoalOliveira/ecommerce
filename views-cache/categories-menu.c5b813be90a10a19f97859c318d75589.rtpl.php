b15846bb89fa0d0d9e7730c1f94f5954473bdc49
Paschoal <paschoaloliveira@hotmail.com>
2019-08-30 10:58:05 -0300

    Site - Categorias

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/8">Apple</a></li><li><a href="/categories/4">google 3</a></li><li><a href="/categories/5">google 3</a></li><li><a href="/categories/7">SamSumg</a></li>
