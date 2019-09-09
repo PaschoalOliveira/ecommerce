49bf03fd2d13823d98f0363401e26ba97973bd2b
Paschoal <paschoaloliveira@hotmail.com>
2019-09-09 17:06:11 -0300

    Inicio carrinho

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -14,6 +14,44 @@ class User extends Model{
 	const SECRET = "HcodePhp7_Secret";
 	const SECRET_IV = "HcodePhp7_Secret_IV";
 
+
+
+	public static function getFromSession()
+	{
+		$user = new User();
+		if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION] >0 )
+		{
+			$user->setData($_SESSION[User::SESSION]);
+		}
+		return $user;
+	}
+
+	public static function checkLogin($inadmin = true)
+	{
+		if(!isset($_SESSION[User::SESSION])
+			|| 
+			!$_SESSION[User::SESSION] 
+			||
+			!((int)$_SESSION[User::SESSION]["iduser"]) > 0)
+		{
+			//Não está logado
+			return false;
+		}else
+		{
+			if($inadmin ===true && (bool)$_SESSION[User::SESSION]["iduser"] ===true)
+			{
+				return true;
+
+			}else if($inadmin ===false)
+			{
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+	}
+
 	public static function login($login, $password)
 	{
 		$sql = new Sql();
@@ -51,15 +89,7 @@ class User extends Model{
 
 	public static function verifyLogin($inadmin = true)
 	{
-		if(!isset($_SESSION[User::SESSION])
-			|| 
-			!$_SESSION[User::SESSION] 
-			||
-			!((int)$_SESSION[User::SESSION]["iduser"]) > 0
-			|| 
-			(boolean)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-		)
-
+		if(User::checkLogin($inadmin))
 		{
 			header("Location: /admin/login");
 			exit;
